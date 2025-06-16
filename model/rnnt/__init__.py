42f7551cd3cbcaa8b307543cf2c6018f89c7f355
CentaureaHO <tangxianda15@outlook.com>
2025-06-16 17:07:03 +0800

    Remove false impl

--- model/rnnt/__init__.py
+++
@@ -1,9 +0,0 @@
-from model.rnnt.transducer import Transducer
-from model.rnnt.predictor import RNNPredictor, PredictorBase
-from model.rnnt.joint import TransducerJoint
-from model.rnnt.encoder import StreamingEncoder
-
-__all__ = [
-    'Transducer', 'RNNPredictor', 'PredictorBase', 'TransducerJoint',
-    'StreamingEncoder'
-]
